2ee22313c0006a00f0519dab3e9c3e2f7b5ba8ae
HanseLopez99 <53914213+HanseLopez99@users.noreply.github.com>
2020-06-06 02:26:10 -0600

    Se agrego clase UserRepository
    sirve para acceder a los metodos de UserDAO

---
+++ app/src/main/java/com/example/enfocat/db/UserRepository.kt
@@ -0,0 +1,21 @@
+package com.example.enfocat.db
+
+class UserRepository(private val dao : UserDAO) {
+    val users = dao.getAllUsers()
+
+    suspend fun insert(user: User){
+        dao.insertUser(user)
+    }
+
+    suspend fun update(user: User){
+        dao.updateUser(user)
+    }
+
+    suspend fun  delete(user: User){
+        dao.deleteUser(user)
+    }
+
+    suspend fun  deleteAll(){
+        dao.deleteAll()
+    }
+}
