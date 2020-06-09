1a163cbf3f1e56a94d6abe4a4aed782985eaf7b5
Oscar De Leon <owdelc@gmail.com>
2020-06-08 21:03:24 -0600

    email login implementado

--- app/src/main/java/com/example/enfocat/MainActivity.kt
+++ app/src/main/java/com/example/enfocat/MainActivity.kt
@@ -14,6 +14,7 @@ import android.os.PersistableBundle
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.analytics.FirebaseAnalytics
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -67,7 +68,6 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
 
 
--- app/src/main/java/com/example/enfocat/ProfileFragment.kt
+++ app/src/main/java/com/example/enfocat/ProfileFragment.kt
@@ -7,12 +7,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
-
+import kotlinx.android.synthetic.main.fragment_profile.*
 /**
  * A simple [Fragment] subclass.
  */
+
+enum class ProviderType{
+    BASIC
+}
 class ProfileFragment : Fragment() {
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,20 +29,45 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val vv = inflater.inflate(R.layout.fragment_profile, fragmentContainer, false)
-        val btnSettings= vv.findViewById<View>(R.id.btn_settings) as Button
+        val btnSettings = vv.findViewById<View>(R.id.btn_settings) as Button
+
+
+
+
+
 
         btnSettings.setOnClickListener{
             val intent = Intent(this@ProfileFragment.context,settings::class.java)
             startActivity(intent)
 
+            val bundle = intent.extras
+            val email= bundle?.getString("email")
+            setup(email ?: "")
+        }
+        return vv}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+    private fun setup(email: String) {
+        emailText.text = email
+
+        LogOutButton.setOnClickListener{
+            FirebaseAuth.getInstance().signOut()
+
         }
 
 
-        return vv
 
-    }
+}
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        btn_signUp.setOnClickListener{
+            findNavController().navigate(R.id.action_profileFragment_to_authentication)
+        }
     }
 }
+
